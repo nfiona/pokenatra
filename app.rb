2f5d5a81f38aceead004b5444f3b449bed7d8810
Fiona <h.natapra@gmail.com>
2017-02-15 09:53:14 -0500

    update & edit

--- app.rb
+++ app.rb
@@ -20,3 +20,19 @@ get '/pokemons/:id' do
   @pokemon = Pokemon.find(params[:id])
   erb :"pokemons/show"
 end
+
+get '/pokemons/:id/:edit' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemons/edit"
+end
+
+put '/pokemons/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.update(params[:pokemon])
+  redirect "pokemons/#{@pokemon.id}"
+end
+
+post '/pokemons' do
+  @pokemon = Pokemon.create(params[:pokemon])
+  redirect "/pokemons/#{@pokemon.id}"
+end
